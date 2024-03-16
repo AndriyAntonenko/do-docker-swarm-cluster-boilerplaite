b53d507ca94f8ecb8d010b75990638ace16b2eb3
Andrii Antonenko <andriyantonenko3.16@gmail.com>
2024-03-16 17:05:55 +0200

    feat: master_node_private_ip as output variable

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -1,3 +1,7 @@
 output "master_ip" {
   value = digitalocean_droplet.master.ipv4_address
 }
+
+output "master_private_ip" {
+  value = digitalocean_droplet.master.ipv4_address_private
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "master_node_ip" {
   value       = module.do_swarm_cluster.master_ip
   description = "IP address of the master node, could be used to connect to the cluster"
 }
+
+output "master_node_private_ip" {
+  value       = module.do_swarm_cluster.master_private_ip
+  description = "Private IP address of the master node, could be used to connect to the cluster from private vpc"
+
+}
